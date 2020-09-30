094e71f12fc18b46c65e79e20cf92ea92c23b2f4
Ken Sodemann <ken@ionic.io>
2020-09-30 16:16:27 -0500

    chore(): upgrade dependencies

--- src/app/about/about.page.ts
+++ src/app/about/about.page.ts
@@ -1,18 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { EMPTY } from 'rxjs';
 import { ToastController } from '@ionic/angular';
+import { AuthMode } from '@ionic-enterprise/identity-vault';
+
+import { dependencies } from '../../../package.json';
+
 import { AuthenticationService } from '../services/authentication';
 import { IdentityService } from '../services/identity';
 import { User } from '../models/user';
-import { catchError } from 'rxjs/operators';
-import { EMPTY } from 'rxjs';
-import { AuthMode } from '@ionic-enterprise/identity-vault';
 
 @Component({
   selector: 'app-about',
   templateUrl: 'about.page.html',
   styleUrls: ['about.page.scss'],
 })
-export class AboutPage {
+export class AboutPage implements OnInit {
+  angularVersion: string;
+  capacitorVersion: string;
+  frameworkVersion: string;
+  identityVaultVersion: string;
+
   user: User;
   authMode: string;
   supportedHardware: string;
@@ -27,6 +35,19 @@ export class AboutPage {
     this.identity.get().subscribe(u => (this.user = u));
     this.authMode = AuthMode[await this.identity.getAuthMode()];
     this.supportedHardware = await this.identity.supportedBiometricTypes();
+  }
+
+  ngOnInit() {
+    const verSpec = /[\^~]/;
+    this.angularVersion = dependencies['@angular/core'].replace(verSpec, '');
+    this.capacitorVersion = dependencies['@capacitor/core'].replace(
+      verSpec,
+      '',
+    );
+    this.frameworkVersion = dependencies['@ionic/angular'].replace(verSpec, '');
+    this.identityVaultVersion = dependencies[
+      '@ionic-enterprise/identity-vault'
+    ].replace(verSpec, '');
   }
 
   logout() {
